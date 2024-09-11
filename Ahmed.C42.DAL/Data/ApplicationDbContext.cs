0efa04aa89c5c4f827a32e6d67cd7fd6deccb766
Saeeed0 <96127674+Saeeed0@users.noreply.github.com>
2024-09-11 17:10:16 +0300

    DAL for Department Module

---
+++ Ahmed.C42.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Ahmed.C42.DAL.Data.Configurations;
+using Ahmed.C42.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahmed.C42.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .; Database = MVCApplication; Trusted_Connection = True; MultipleActiveResultsSets = false;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Ahmed.C42.DAL/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahmed.C42.DAL.Models
+{
+    //Model Represent a Department Table
+    internal class Department
+    {
+        public int Id { get; set; }
+        [Required/*(ErrorMessage ="Code is Required ya Hamada!!")*/]//ErrorMessage Should inside View Modle
+        public string Code { get; set; }
+        //[Required]///We prefer select Required or any DataAnotation inside fluent API
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
