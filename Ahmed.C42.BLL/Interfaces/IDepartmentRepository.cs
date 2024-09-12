1c5aeb2c1664bb60c4f986be7c9cff833dae5a09
Saeeed0 <96127674+Saeeed0@users.noreply.github.com>
2024-09-12 10:04:33 +0300

    Part 08 3-Layers Arch. Pattern - Business Logic Layer

---
+++ Ahmed.C42.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ahmed.C42.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahmed.C42.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        //List<Department> GetAll();
+        IEnumerable<Department> GetAll();
+        Department Get(int Id);
+        int Add(Department entity);
+        int Update(Department entity);
+        int Delete(Department entity);
+        //Department Update(Department entity);
+        //Department Delete(int id);
+    }
+}
---
+++ Ahmed.C42.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,55 @@
+﻿using Ahmed.C42.BLL.Interfaces;
+using Ahmed.C42.DAL.Data;
+using Ahmed.C42.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahmed.C42.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        readonly ApplicationDbContext _dbContext;//Null
+        //public DepartmentRepository()//without using DI
+        //{
+        //    _dbContext = new ApplicationDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext>());
+        //}
+        public DepartmentRepository(ApplicationDbContext applicationDbContext)
+        {
+            _dbContext = applicationDbContext;
+        }
+        public int Add(Department entity)
+        {
+            _dbContext.Departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Department Get(int Id)
+        {
+            return _dbContext.Find<Department>(Id);//find first in Local if don't exist find in DB
+            //return _dbContext.Departments.Find(Id);//find first in Local if don't exist find in DB
+            //var department = _dbContext.Departments.Local.Where(D => D.Id == Id).FirstOrDefault();
+            //if (department == null) department = _dbContext.Departments.Where(D => D.Id == Id).FirstOrDefault();
+            //return department;
+        }
+
+        public IEnumerable<Department> GetAll()
+            => _dbContext.Departments.AsNoTracking().ToList();
+
+
+
+    }
+}
